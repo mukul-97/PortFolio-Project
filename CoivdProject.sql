dba4be5d5e9064209632735ae3a7558fadc944b9
mukul dabi <mukul.dabi@gmail.com>
2024-05-19 19:24:52 +0530

    Add files via upload

--- CoivdProject.sql
+++ CoivdProject.sql
@@ -1,14 +1,23 @@
-select count(*) from CovidDeaths
-select count(*) from CovidVaccinations
-select * from CovidDeaths
-select * from CovidVaccinations
-truncate table covidvaccinations
-ALTER TABLE CovidDeaths
-ALTER COLUMN total_deaths float;
+/*
+Covid 19 Data Exploration 
 
-select location,date,total_cases,new_cases,total_deaths,population
-from CovidDeaths
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+
+*/
+
+Select *
+From PortfolioProject..CovidDeaths
+Where continent is not null 
+order by 3,4
+
+
+-- Select Data that we are going to be starting with
+
+Select Location, date, total_cases, new_cases, total_deaths, population
+From PortfolioProject..CovidDeaths
+Where continent is not null 
 order by 1,2
+
 
 -- Looking at Total Cases vs Total Deaths
 -- Showing Likelihood of dying if you contract covid in your country

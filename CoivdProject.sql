49d2cd52ce8483ecdebf79e8ba17dcda71fc2e6d
mukul dabi <mukul.dabi@gmail.com>
2024-05-20 18:42:09 +0530

    Add files via upload

--- CoivdProject.sql
+++ CoivdProject.sql
@@ -88,7 +88,7 @@ where  continent is not null
 
 -- Looking at Total Population vs Vaccinations
 
-Select dea.continent,dea.location,dea.date,dea.population,vac.new_vaccinations, 
+Select dea.continent,dea.location,dea.date,dea.population,vac.total_vaccinations,vac.new_vaccinations, 
 SUM(cast(vac.new_vaccinations as bigint)) over(partition by dea.location order by dea.location,dea.date) as Rolling_Count_Vaccinatied_People
 from 
 CovidDeaths dea
@@ -158,3 +158,66 @@ where dea.continent is not null
 Select 
 	* 
 from VW_Rolling_Count_Vaccinatied_People
+
+
+
+
+/*
+
+Queries used for Tableau Project
+
+*/
+
+
+
+-- 1. 
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, 
+SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as Death_Percentage
+From CovidDeaths
+where continent is not null 
+order by 1,2
+
+
+
+
+-- 2. 
+
+-- We take these out as they are not inluded in the above queries and want to stay consistent
+
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From CovidDeaths
+Where continent is null 
+and location not in ('World', 'European Union', 'International','High income','Upper middle income','Lower middle income','Low income')
+Group by location
+order by TotalDeathCount desc
+
+
+-- 3.
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidDeaths
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+select location,population,Max(total_cases) as Highest_Infeaction_counts,( max( total_cases)/population )*100 as Percentage_Of_Population_Effected
+from CovidDeaths
+where continent is not null
+group by location,population
+order by Percentage_Of_Population_Effected desc
+-- 4.
+
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidDeaths
+Group by Location, Population, date
+order by PercentPopulationInfected desc
+
+
+
+
+
+
+

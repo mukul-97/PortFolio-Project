f883abfc4333772358564f56e1edc80df083f6db
mukul dabi <mukul.dabi@gmail.com>
2025-12-16 23:11:32 +0530

    Refactor SQL queries for Covid data analysis

--- CoivdProject.sql
+++ CoivdProject.sql
@@ -5,16 +5,13 @@ Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, 
 
 */
 
-Select *
-From PortfolioProject..CovidDeaths
-Where continent is not null 
-order by 3,4
+
 
 
 -- Select Data that we are going to be starting with
 
 Select Location, date, total_cases, new_cases, total_deaths, population
-From PortfolioProject..CovidDeaths
+From CovidDeaths
 Where continent is not null 
 order by 1,2
 
@@ -214,6 +211,7 @@ Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Ma
 From CovidDeaths
 Group by Location, Population, date
 order by PercentPopulationInfected desc
+
 
 
 
